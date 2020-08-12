adc009bfe6a23866762e6b7fedd11bb501d91806
Daniel Collins <dcollins@afs.net>
2020-08-12 00:02:03 -0400

    Provide ability to add a rule...just one though, more than one is broken

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,19 @@
         <h2>Custom Interface for Search and Filter</h2>
         <p>DMN Name: {{ this.dmnName }}</p>
         <p>Hit Policy: {{ this.hitPolicy }}</p>
+        <el-button
+          type="primary"
+          style="float: right; margin-bottom: 18px;"
+          @click="onDisplayAddRule"
+        >
+          Add Rule
+        </el-button>
         <el-table
           :data="customTableData"
           :header-cell-class-name="cellClassNameAssigner"
           :cell-class-name="cellClassNameAssigner"
+          highlight-current-row
+          @current-change="onCurrentRowChange"
           style="width: 100%"
         >
           <el-table-column
@@ -29,6 +38,18 @@
             filter-multiple
           />
         </el-table>
+        <el-dialog :visible.sync="ruleEditorVm.visible">
+          <el-form>
+            <el-form-item
+              v-for="column in customTableColumns"
+              :key="column.id"
+              :label="column.label"
+            >
+              <el-input v-model="ruleEditorVm.rule[column.id]" />
+            </el-form-item>
+            <el-button type="primary" @click="onAddRule">Add Rule</el-button>
+          </el-form>
+        </el-dialog>
       </el-tab-pane>
       <el-tab-pane label="Preview">
         <h2>Camunda DMN Preview</h2>
@@ -49,6 +70,20 @@ const ColumnTypes = Object.freeze({
   INPUT: 'dmn:InputClause',
   OUTPUT: 'dmn:OutputClause',
 });
+
+const MODDLE_INSTANCE = new DmnModdle();
+
+// TODO: Element id attributes need a unique value. This is good enough for this playground
+function goodEnoughUidGenerator(length = 8) {
+  var result = '';
+  var characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 export default {
   ColumnTypes,
@@ -130,9 +165,13 @@ export default {
         height: '100%',
         width: '100%',
       }),
-      moddle: new DmnModdle(),
       xmlData: LineItemMappingDmn,
       moddleDefinitions: undefined,
+      currentRow: undefined,
+      ruleEditorVm: {
+        visible: false,
+        rule: {},
+      },
     };
   },
   mounted() {
@@ -166,7 +205,7 @@ export default {
     },
     async syncModdleXml() {
       return new Promise((resolve, reject) => {
-        this.moddle.fromXML(this.xmlData, DMN_ROOT, (err, definitions) => {
+        MODDLE_INSTANCE.fromXML(this.xmlData, DMN_ROOT, (err, definitions) => {
           if (err) {
             console.error('error initing moddle', err);
             reject(err);
@@ -198,6 +237,70 @@ export default {
     },
     filterMethod(columnId) {
       return (value, row) => row[columnId].text === value;
+    },
+    onCurrentRowChange(currentRow) {
+      this.currentRow = currentRow;
+    },
+    onDisplayAddRule() {
+      this.ruleEditorVm.visible = true;
+
+      // initialize ruleEditor object properties so they are reactive
+      this.ruleEditorVm.rule = this.customTableColumns.reduce(
+        (resultingObject, column) => {
+          resultingObject[column.id] = '';
+          return resultingObject;
+        },
+        {}
+      );
+    },
+    onAddRule() {
+      const rule = this.ruleEditorVm.rule;
+
+      const inputEntries = this.customTableColumns
+        .filter(column => column.$type === ColumnTypes.INPUT)
+        .map(column =>
+          MODDLE_INSTANCE.create('dmn:UnaryTests', {
+            id: `UnaryTests_${goodEnoughUidGenerator()}`,
+            text: rule[column.id],
+          })
+        );
+
+      const outputEntries = this.customTableColumns
+        .filter(column => column.$type === ColumnTypes.OUTPUT)
+        .map(column =>
+          MODDLE_INSTANCE.create('dmn:LiteralExpression', {
+            id: `LiteralExpression_${goodEnoughUidGenerator()}`,
+            text: rule[column.id],
+          })
+        );
+
+      const ruleElement = MODDLE_INSTANCE.create('dmn:DecisionRule', {
+        id: `DecisionRule_${goodEnoughUidGenerator()}`,
+        inputEntry: inputEntries,
+        outputEntry: outputEntries,
+      });
+
+      console.info('rule', ruleElement);
+      this.moddleDefinitions?.drgElement?.[0].decisionLogic.rule.push(
+        ruleElement
+      );
+      // not the right way to do it, but good enough for playground
+      this.updateCamundaOfModdleDefinitionChange();
+    },
+    async updateCamundaOfModdleDefinitionChange() {
+      return new Promise((resolve, reject) => {
+        MODDLE_INSTANCE.toXML(this.moddleDefinitions, {}, (err, xml) => {
+          if (err) {
+            console.error('error writing moddle definitions to xml', err);
+            reject(err);
+          }
+
+          console.log('generated xml', xml);
+          this.xmlData = xml;
+          this.syncCamundaXml();
+          resolve(xml);
+        });
+      });
     },
   },
 };
